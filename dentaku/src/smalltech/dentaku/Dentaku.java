0d714830fda2662fe2484b58b27fde1800e561bd
smalltech-bw <smalltech.bw@gmail.com>
2020-04-27 21:20:42 +0900

    電卓アプリのGitでの管理を開始。

---
+++ dentaku/src/smalltech/dentaku/Dentaku.java
@@ -0,0 +1,73 @@
+package smalltech.dentaku;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+
+public class Dentaku extends JFrame{
+	
+	JPanel contentPane = new JPanel();
+	BorderLayout borderLayout1 = new BorderLayout();
+	JTextField result_text_field = new JTextField(""); 
+	
+	public Dentaku() {
+		//ウィンドウについての設定
+		this.setTitle("dentaku");
+		this.setSize(300, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane.setLayout(borderLayout1);
+		this.setContentPane(contentPane);
+		
+		//出力用テキストフィールド
+		contentPane.add(result_text_field, BorderLayout.NORTH); 
+		
+		//ボタン部分
+		JPanel keyPanel = new JPanel();
+		keyPanel.setLayout(new GridLayout(4, 4)); 
+		contentPane.add(keyPanel, BorderLayout.CENTER);
+		
+		keyPanel.add(new NumButton(7), 0); 
+		keyPanel.add(new NumButton(8), 1); 
+		keyPanel.add(new NumButton(9), 2); 
+		keyPanel.add(new JButton("÷"), 3);
+		keyPanel.add(new NumButton(4), 4);
+		keyPanel.add(new NumButton(5), 5);
+		keyPanel.add(new NumButton(6), 6); 
+		keyPanel.add(new JButton("×"), 7);
+		keyPanel.add(new NumButton(1), 8);
+		keyPanel.add(new NumButton(2), 9);
+		keyPanel.add(new NumButton(3), 10);
+		keyPanel.add(new JButton("－"), 11);
+		keyPanel.add(new NumButton(0), 12); 
+		keyPanel.add(new JButton("."), 13);
+		keyPanel.add(new JButton("＋"), 14);
+		keyPanel.add(new JButton("＝"), 15);
+		
+		contentPane.add(new JButton("C"), BorderLayout.SOUTH);
+		this.setVisible(true);
+		
+	}
+	
+	public void appendResult(String c) {
+		result_text_field.setText(result_text_field.getText() + c);
+	}
+	
+	public class NumButton extends JButton implements ActionListener {
+		private int button_num;
+		
+		public NumButton(int num) {
+			super(Integer.toString(num));
+			button_num = num;
+			this.addActionListener(this); 
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			String keyNumber = Integer.toString(button_num);
+			appendResult(keyNumber);
+		}
+	}
+
+}
---
+++ dentaku/src/smalltech/dentaku/Starter.java
@@ -0,0 +1,9 @@
+package smalltech.dentaku;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		new Dentaku();
+	}
+
+}
