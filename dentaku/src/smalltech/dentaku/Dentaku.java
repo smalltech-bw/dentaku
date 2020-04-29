2efa8a34b55684d612558d67e55a71bda80b2c89
smalltech <smalltech.bw@gmail.com>
2020-04-29 19:30:40 +0900

    CalcButtonを追加した。ビープ音、警告ダイアログも追加して少し遊んだ。

--- dentaku/src/smalltech/dentaku/Dentaku.java
+++ dentaku/src/smalltech/dentaku/Dentaku.java
@@ -11,6 +11,8 @@ public class Dentaku extends JFrame{
 	JPanel contentPane = new JPanel();
 	BorderLayout borderLayout1 = new BorderLayout();
 	JTextField result_text_field = new JTextField(""); 
+	private String dentakuState;
+	private double firstNum;
 	
 	public Dentaku() {
 		//ウィンドウについての設定
@@ -32,18 +34,18 @@ public class Dentaku extends JFrame{
 		keyPanel.add(new NumButton(7), 0); 
 		keyPanel.add(new NumButton(8), 1); 
 		keyPanel.add(new NumButton(9), 2); 
-		keyPanel.add(new JButton("÷"), 3);
+		keyPanel.add(new CalcButton("÷"), 3);
 		keyPanel.add(new NumButton(4), 4);
 		keyPanel.add(new NumButton(5), 5);
 		keyPanel.add(new NumButton(6), 6); 
-		keyPanel.add(new JButton("×"), 7);
+		keyPanel.add(new CalcButton("×"), 7);
 		keyPanel.add(new NumButton(1), 8);
 		keyPanel.add(new NumButton(2), 9);
 		keyPanel.add(new NumButton(3), 10);
-		keyPanel.add(new JButton("－"), 11);
+		keyPanel.add(new CalcButton("－"), 11);
 		keyPanel.add(new NumButton(0), 12); 
 		keyPanel.add(new JButton("."), 13);
-		keyPanel.add(new JButton("＋"), 14);
+		keyPanel.add(new CalcButton("＋"), 14);
 		keyPanel.add(new JButton("＝"), 15);
 		
 		contentPane.add(new JButton("C"), BorderLayout.SOUTH);
@@ -67,6 +69,23 @@ public class Dentaku extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			String keyNumber = Integer.toString(button_num);
 			appendResult(keyNumber);
+		}
+	}
+	
+	public class CalcButton extends JButton implements ActionListener {
+		private String calcKind;
+		public CalcButton(String kind) {
+			super(kind);		
+			calcKind = kind;
+			this.addActionListener(this); 
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			dentakuState = calcKind;
+			firstNum = Double.parseDouble(result_text_field.getText());
+			java.awt.Toolkit.getDefaultToolkit().beep();//ビープ音お試し
+			JOptionPane.showMessageDialog(Dentaku.this, "calc button pushed");//警告表示お試し
+			System.out.println("calc button pushed");
 		}
 	}
 
